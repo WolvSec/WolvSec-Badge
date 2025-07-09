9fad0dc2fa243cfa685cc09ccc3a7ef80c74a486
Marshall Stone <stmr@umich.edu>
2025-07-08 22:34:21 -0400

    lcd interface: refactor command handling

--- include/lcd_impl.h
+++ include/lcd_impl.h
@@ -21,21 +21,27 @@
 #define LCD_PIN_DC 6
 
 #define SPI_PORT spi0
-#define LCD_SPI_SPEED 1000000 // ?
+#define LCD_SPI_SPEED 1000000
 
-/* Initialize communication with the LCD. */
-void lcd_spi_init();
+typedef struct lcd_user_data {
+    /* DMA configuration. */
+    int dma_chan;
+    dma_channel_config dma_conf;
+} lcd_user_data;
 
-/* Set our LCD to simplify IRQ callbacks. */
-void lcd_set_ctx(lv_display_t *lcd);
+/* Initialize communication interfaces for the LCD display. */
+void lcd_init();
+
+/* Enable data transfers using DMA. */
+void lcd_dma_init(lv_display_t *lcd);
 
 /* Send short command to the LCD. This function shall wait until the transaction finishes. */
-int32_t my_lcd_send_cmd(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, const uint8_t *param, size_t param_size);
+int32_t lcd_send_cmd(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, const uint8_t *param, size_t param_size);
 
 /* Send large array of pixel data to the LCD. Use DMA. */
-int32_t my_lcd_send_color_dma(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, uint8_t *param, size_t param_size);
+int32_t lcd_send_color_dma(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, uint8_t *param, size_t param_size);
 
 /* Send large array of pixel data to the LCD. Should only be used in cases where DMA is not an option. */
-int32_t my_lcd_send_color(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, uint8_t *param, size_t param_size);
+int32_t lcd_send_color(lv_display_t *disp, const uint8_t *cmd, size_t cmd_size, uint8_t *param, size_t param_size);
 
 #endif
--- src/wolvsec_badge.c
+++ src/wolvsec_badge.c
@@ -60,14 +60,15 @@ void ui_init(lv_display_t *disp)
 
 int pico_display_init(void) {
     lv_init();
-    lcd_spi_init();
+    lcd_init();
     add_repeating_timer_ms(5, repeating_lvgl_timer_callback, NULL, &lvgl_timer);
-    lcd = lv_st7789_create(LCD_RES_X, LCD_RES_Y, 0, (lv_st7789_send_cmd_cb_t) my_lcd_send_cmd, (lv_st7789_send_color_cb_t) my_lcd_send_color_dma);
+    lcd = lv_st7789_create(LCD_RES_X, LCD_RES_Y, 0, (lv_st7789_send_cmd_cb_t) lcd_send_cmd, (lv_st7789_send_color_cb_t) lcd_send_color_dma);
+    lcd_dma_init(lcd);
     if (!lcd) {
         printf("lv_st7789_create failed with code: %d\n", lcd);
         return -1;
     }
-    lcd_set_ctx(lcd);
+    //lcd_init(lcd);
     buf_size = LCD_RES_X * LCD_RES_Y / 10 * lv_color_format_get_size(lv_display_get_color_format(lcd));
     buf1 = lv_malloc(buf_size);
     if(buf1 == NULL) {
@@ -83,7 +84,6 @@ int pico_display_init(void) {
     }
     lv_display_set_buffers(lcd, buf1, buf2, buf_size, LV_DISPLAY_RENDER_MODE_PARTIAL);
     lv_st7789_set_invert(lcd, 1);
-    //  ui_init(lcd);
     ui_logo_init();
     return PICO_OK;
 }
