68b16af5fc7a6889075c54217abfef79fe3f9ed9
Marshall Stone <stmr@umich.edu>
2026-01-25 17:09:21 -0500

    Support USB storage devices mounted under /run for deploying uf2 binary

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-cp build/wolvsec_badge.uf2 /media/**/RP2350
+PICO_PATH=""
+
+if [[ -f /media ]]; then
+    PICO_PATH="/media/**/RP2350"
+else
+    PICO_PATH="/run/media/**/RP2350"
+fi
+
+cp build/wolvsec_badge.uf2 $PICO_PATH
 
